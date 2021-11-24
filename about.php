2f3f344b3fea9830a0cc809ff412672d7ff49ef6
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-11-24 12:18:56 -0700

    Create about.php
    Creating the outline for the next page of the website.

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Drew Cring Training</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+        <meta name="description" content="Drew Cring's Personal Training website for clients and users to contact and learn about Drew's expierience and services">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+
+    </head>
+    <body>
+        <?php
+
+        ?>
+    <img id="banner" src="images/DCPTLogo.png" alt="Drew Cring's Personal Training"/>
+    <nav>
+        <ul>
+            <li><a href="about">About</a></li>
+            <li><a href="online">Online Training</a></li>
+            <li><a href="articles">Articles</a></li>
+            <li><a href="contact">Contact</a></li>
+            <li><a href="programs">Programs</a></li>
+        </ul>
+    </nav>
+      
+      
+     <script src="" async defer></script>
+    <footer>
+    <img id="smallLogo" src="images/DCPTLogoSmall.png" alt="Drew Cring's Personal Training Abreviation Logo"/>
+        <nav>
+            <ul>
+                <li><a href="about">About</a></li>
+                <li><a href="online">Online Training</a></li>
+                <li><a href="articles">Articles</a></li>
+                <li><a href="contact">Contact</a></li>
+                <li><a href="programs">Programs</a></li>
+            </ul>
+        </nav>
+      <p>Created By Andrew Cring</p>
+    </footer>
+    </body>
+</html>
