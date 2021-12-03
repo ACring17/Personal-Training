60b90baa678871e3a2ce3d2123ee3897360e5ecf
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-12-03 10:14:34 -0700

    Update about.php
    Adding a home link for the nav

--- about.php
+++ about.php
@@ -19,6 +19,7 @@
     <img id="banner" src="images/DCPTLogo.png" alt="Drew Cring's Personal Training"/>
     <nav>
         <ul>
+            <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="online">Online Training</a></li>
             <li><a href="articles">Articles</a></li>
@@ -42,6 +43,7 @@
     <img id="smallLogo" src="images/DCPTLogoSmall.png" alt="Drew Cring's Personal Training Abreviation Logo"/>
         <nav>
             <ul>
+                <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="online">Online Training</a></li>
                 <li><a href="articles">Articles</a></li>
