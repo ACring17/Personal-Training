7e9f834831f1080fa86d55f74b9a154c5a5a310d
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-12-07 11:23:44 -0700

    Update index.php
    Edit the main body content on word choice

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     <h2>Drew is here to help with your workout needs!</h2>
         <p>Drew Cring is ready to helo you acheive your personal health and fitness goals. With a bachelor's of science degree in exercise science and a long history with sports and fitness training, Drew will be there for you with expiernce and support to help you through adversity. You do not have to go through this journey alone.</p>
             <br/>
-        <p>Drew's main area of focus is exercise and movement. He'll be the person you need to hold you accountable to show up for your workouts. He will prescribe exercises that will help you train to achieve your goals.</p>
+        <p>Drew's main area of expertice is exercise science and movement training. With over a decade of practice and knowledge he can answer any questions you may have. He'll be the person you need to hold you accountable to show up for your workouts. He will prescribe exercises that will help you train to achieve your goals.</p>
         <p>Please enjoy exploring this website and learn more about Drew and what he can do for you to help you acheive the best version of your self.</p>
     </div>
             
