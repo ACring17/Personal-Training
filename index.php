965d808e3d84b406c4649f17f7d226ec08044e51
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-12-13 13:29:21 -0700

    Update index.php
    Adding some size rules for the small logo at the bottom

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             
         <!--<img photo of Me/> ...  Maybe a carousel of training with clients -->
     <footer>
-        <img id="smallLogo" src="images/DCPTLogoSmall.png" alt="Drew Cring's Personal Training Abreviation Logo"/>
+        <img id="smallLogo" src="images/DCPTLogoSmall.png" width="250" height="184" alt="Drew Cring's Personal Training Abreviation Logo"/>
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
