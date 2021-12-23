b7833ee4251a058a790f4a11fc44eb09285a5e5a
Drew Cring <66842994+ACring17@users.noreply.github.com>
2021-12-23 10:24:51 -0700

    Create programs.php
    Laying out the basic outline for the programs page

---
+++ programs.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Drew Cring Training</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+        <meta name="description" content="Drew Cring's Personal Training website for clients and users to contact and learn about Drew's expierience and services">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+
+    </head>
+    <body>
+        <?php
+
+        ?>
+    <img id="banner" src="images/DCPTLogoHiRes.png" alt="Drew Cring's Personal Training"/>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="programs">Programs</a></li>
+            <li><a href="online">Online Training</a></li>
+            <li><a href="articles">Articles</a></li>
+            <li><a href="contact">Contact</a></li>
+        </ul>
+    </nav>
+      <h1>Training Programs</h1>
+      <br/>
+      
+      
+     <script src="" async defer></script>
+    <footer>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="programs">Programs</a></li>
+                <li><a href="online">Online Training</a></li>
+                <li><a href="articles">Articles</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+        </nav>
+        <img id="smallLogo" src="images/DCPTLogoSmall.png" alt="Drew Cring's Personal Training Abreviation Logo"/>
+      <p>Created By Andrew Cring</p>
+    </footer>
+    </body>
+</html>
