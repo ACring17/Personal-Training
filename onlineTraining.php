ceca637c346c3350b2fee01c3ab3e90a02bf47d9
Drew Cring <66842994+ACring17@users.noreply.github.com>
2022-01-13 12:08:24 -0700

    Update onlineTraining.php
    Made a few adjustments to the paragraph about the online training

--- onlineTraining.php
+++ onlineTraining.php
@@ -36,7 +36,7 @@
         <h1>Online Personal Training</h1>
             <h2>Personalized personal training in your own home</h2>
         <img id="banner" src="images/onlineTrain.png" alt="Online Training badge"/>
-                <p>With COVID-19 making close contact a health concern, Drew is offering a way to help keep you safe and fit. Through the online personal training program you will have access to Drew's knowledge and expertise with the option to contact whenever you have a question. The online training will come with workout plans that </p>
+                <p>With COVID-19 making close contact a health concern, Drew is offering a way to help keep you safe and fit. Through the online personal training program you will have access to Drew's knowledge and expertise with the option to contact him whenever you have a question or need guideance. The online training will come with workout plans that can be done whenever convient for you. There will be mandatory once a week check-ins to see how things are going. Online training offers a ton of freedom and flexibility to fix your schedule.</p>
     <hr/>
         <h2>Program Packages</h2>
         <div class="row">
