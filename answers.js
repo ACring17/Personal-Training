4b34417f073e533749cc38640aeb2e52d12b4502
Drew Cring <66842994+ACring17@users.noreply.github.com>
2022-01-18 11:35:14 -0700

    Create answers.js
    Adding the functions for the answers onClick function

---
+++ answers.js
@@ -0,0 +1,15 @@
+$("#question1").click(function() {
+    $("#answer1").toggle("h3");
+});
+
+$("#question2").click(function() {
+    $("#answer2").toggle("h3");
+});
+
+$("#question3").click(function() {
+    $("#answer3").toggle("h3");
+});
+
+$("#question4").click(function() {
+    $("#answer4").toggle("h3");
+});
