2261a40d6c0971af33168a1d1b03b372334df630
Drew Cring <66842994+ACring17@users.noreply.github.com>
2022-01-24 10:57:53 -0700

    Create articles.php
    Adding the skeleton for the articles page

---
+++ articles.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Drew Cring Training Articles</title>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+        <meta name="description" content="Drew Cring's Personal Training website for clients and users to contact and learn about Drew's expierience and services">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400;700&family=Roboto&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <?php
+        
+
+        ?>
+        
+    <img id="banner" src="images/DCPTLogoHiRes.png" width="375" height="109" alt="Drew Cring's Personal Training"/>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="programs.php">Programs</a></li>
+            <li><a href="onlineTraining.php">Online Training</a></li>
+            <li><a href="articles">Articles</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+
+    <footer>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="programs.php">Programs</a></li>
+            <li><a href="onlineTraining.php">Online Training</a></li>
+            <li><a href="articles">Articles</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+        <img id="smallLogo" src="images/DCPTLogoSmall.png" width="250" height="184" alt="Drew Cring's Personal Training Abreviation Logo"/>
+      <p>Created By Andrew Cring</p>
+    </footer>
+    <script src="" async defer></script>
+    </body>
+</html>
