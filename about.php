fea83c63976657a4c4325ddc4a8b6e75dad34f9a
Drew Cring <66842994+ACring17@users.noreply.github.com>
2022-02-14 08:55:29 -0700

    Update about.php
    Removed a few IDs and classes on the images to help the page flow better

--- about.php
+++ about.php
@@ -38,13 +38,13 @@
         <img class="leftPhoto" src="images/DrewGrad.jpg" width="375" height="515" alt="Drew's Graduation"/>
       <h2>Work</h2>
         <p>Drew has eight years expierience working as a personal trainer and manager of a fitness boutique using cutting edge fitness equiptment. As a trainer Drew has expierience working with all types of clients from as young as 6 years old to as old as 82 years old and coaching just about every workout style available. Whether it's strength training, functional movement training, weight loss or flexxibility training, Drew has the knowhow to help coach you through it. With a wealth of knowledge to draw from he will be able to help guide you to your ideal self.</p>
-        <img class="rightPhoto" src="images/DrewGrand.jpg" width="375" height="515" alt="Drew at the Grand Canyon"/>
+        <img src="images/DrewGrand.jpg" width="375" height="515" alt="Drew at the Grand Canyon"/>
       <h2>Life Experience</h2>
         <p>Drew has a lifetime of fitness. With morning strength and conditioning workouts in highschool for baseball, running summer sport camps for young kids to learn new skills, coaching high school level baseball, partcipating in mud run obstacle courses, and cycling across the united states for charity, Drew has been through a variety of adversity. No matter what you are training for, Drew will guide you to your goals.</p>
         <img class="leftPhoto" src="images/DrewBike.jpg" width="375" height="515" alt="Drew working hard on a bike"/>
       <h2>Why</h2>
         <p>Drew loves to help others acheive their goals. His ultimate goal is to give his clients the tools nesecarry to thrive on their own and live the lifestyle that will help them be the best version of themselves. Fitness has changed Drew's life and he wants to help others achieve their fitness goals. Through teaching and practice Drew will help you get there.</p>
-        <img id="introPhoto" src="images/DrewMtn.jpg" width="375" height="515" alt="Drew on top of a mountain"/>
+        <img src="images/DrewMtn.jpg" width="375" height="515" alt="Drew on top of a mountain"/>
      <script src="" async defer></script>
     <footer>
         <nav>
